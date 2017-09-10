f5cfa91dcf6177e0198b699784892da0a9970db1
LukeH1993 <lwheudebourck@gmail.com>
2017-09-10 12:24:21 +0100

    Created some prompts for the calculator program

---
+++ calculator.js
@@ -0,0 +1,4 @@
+//Prompts
+var calculator = prompt('Hello, this is your calculator ready to solve your maths problems! Just type "add" to add, "sub" to subtract, "mul" to multiply and "div" to divide! ');
+var firstNum = prompt('Please enter your first number');
+var secondNum = prompt('Please enter your second number');
