8a8824a82ee62647212ae7ba222f8578d6920fd8
LukeH1993 <lwheudebourck@gmail.com>
2017-09-10 12:28:38 +0100

    Created operators for the calculator program

--- calculator.js
+++ calculator.js
@@ -2,3 +2,10 @@
 var calculator = prompt('Hello, this is your calculator ready to solve your maths problems! Just type "add" to add, "sub" to subtract, "mul" to multiply and "div" to divide! ');
 var firstNum = prompt('Please enter your first number');
 var secondNum = prompt('Please enter your second number');
+
+//Operations
+var add = 'add';
+var sub = 'subtract';
+var mul = 'multiply';
+var div = 'divide';
+
